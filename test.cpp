36bc2140cdee468bd0aaba45fb1932c5c5432a70
jkt10125 <jkt10125@gmail.com>
2024-04-17 07:59:59 +0530

    added bug fixes

--- test.cpp
+++ test.cpp
@@ -8,51 +8,40 @@ struct TrieMap {
     const static int btrie_MIN = 0x00000000;
     const static int BTRIE_NODES = 1000000;
 
-    struct node {
-        int c[2];
-        int cnt, par;
-        int value;
-    };
+    // c[2], cnt, par, value
+    std::vector<std::array<int, 5>> btrie;
+    std::vector<int> del;
 
-    node btrie[BTRIE_NODES];
-
-    int del[BTRIE_NODES];
-    int btrie_node_ctr = 1, btrie_del_ctr = 0;
-
-    TrieMap() {
-        std::cout << "hello" << std::endl;
-        btrie[0].par = -1;
-        btrie[0].c[0] = btrie[0].c[1] = btrie[0].cnt = 0;
-        btrie_node_ctr = 1, btrie_del_ctr = 0;
-    }
+    TrieMap() : btrie(1, {0, 0, 0, -1, 0}) { }
 
     int new_node_impl() {
-        int idx = (btrie_del_ctr) ? del[--btrie_del_ctr] : btrie_node_ctr++;
-        btrie[idx].c[0] = btrie[idx].c[1] = btrie[idx].cnt = 0;
+        int idx = (del.empty()) ? btrie.size() : del.back();
+        (del.empty()) ? btrie.push_back({}) : del.pop_back();
+        btrie[idx][0] = btrie[idx][1] = btrie[idx][2] = 0;
         return idx;
     }
 
     void go_up_impl(int &p, int &x) {
-        p = btrie[p].par;
+        p = btrie[p][3];
         x >>= 1;
     }
 
     void go_down_impl(int &p, int &x, int b) {
-        p = btrie[p].c[b];
+        p = btrie[p][b];
         x = x << 1 | b;
     }
 
     int find_impl(int p, int &x, int up, int b) {
         if (up) {
-            while (!((b ^ (x & 1)) && btrie[btrie[p].par].c[b])) {
+            while (!((b ^ (x & 1)) && btrie[btrie[p][3]][b])) {
                 go_up_impl(p, x);
                 if (p == -1) { return 0; }
             }
             go_up_impl(p, x);
             go_down_impl(p, x, b);
         }
-        while (btrie[p].c[0] | btrie[p].c[1]) {
-            go_down_impl(p, x, b ^ (btrie[p].c[1 ^ b] != 0));
+        while (btrie[p][0] | btrie[p][1]) {
+            go_down_impl(p, x, b ^ (btrie[p][1 ^ b] != 0));
         }
         return 1;
     }
@@ -62,34 +51,34 @@ struct TrieMap {
         int p = 0;
         for (int i = btrie_DEPTH; i >= 0; i--) {
             int b = (x >> i) & 1;
-            if (!btrie[p].c[b]) {
-                btrie[p].c[b] = new_node_impl();
-                btrie[btrie[p].c[b]].par = p;
+            if (!btrie[p][b]) {
+                btrie[p][b] = new_node_impl();
+                btrie[btrie[p][b]][3] = p;
             }
-            p = btrie[p].c[b];
+            p = btrie[p][b];
         }
-        if (!btrie[p].cnt) {
-            for (int tp = p; tp != -1; tp = btrie[tp].par) {
-                btrie[tp].cnt++;
+        if (!btrie[p][2]) {
+            for (int tp = p; tp != -1; tp = btrie[tp][3]) {
+                btrie[tp][2]++;
             }
-            btrie[p].value = int();
+            btrie[p][4] = int();
         }
-        return btrie[p].value;
+        return btrie[p][4];
     }
 
     bool erase(int x, int i, int p) {
         x ^= (1 << btrie_DEPTH);
         if (i == -1) {
-            return (btrie[p].cnt) ? (btrie[p].cnt--, 1) : 0;
+            return (btrie[p][2]) ? (btrie[p][2]--, 1) : 0;
         }
         int b = (x >> i) & 1;
-        if (!btrie[p].c[b]) { return 0; }
-        bool ok = erase(x, i - 1, btrie[p].c[b]);
+        if (!btrie[p][b]) { return 0; }
+        bool ok = erase(x, i - 1, btrie[p][b]);
         if (ok) {
-            btrie[p].cnt--;
-            if (!btrie[btrie[p].c[b]].cnt) {
-                del[btrie_del_ctr++] = btrie[p].c[b];
-                btrie[p].c[b] = 0;
+            btrie[p][2]--;
+            if (!btrie[btrie[p][b]][2]) {
+                del.push_back(btrie[p][b]);
+                btrie[p][b] = 0;
             }
         }
         return ok;
@@ -97,7 +86,7 @@ struct TrieMap {
 
     int get_extreme(int b) {
         int p = 0, res = 0;
-        (btrie[0].cnt) ? find_impl(p, res, 0, 1 ^ b) : (res = (b) ? btrie_MIN : btrie_MAX);
+        (btrie[0][2]) ? find_impl(p, res, 0, 1 ^ b) : (res = (b) ? btrie_MIN : btrie_MAX);
         return res ^ (1 << btrie_DEPTH);
     }
 
@@ -106,8 +95,8 @@ struct TrieMap {
         int p = 0, res = 0;
         for (int i = btrie_DEPTH; i >= 0; i--) {
             int bit = (x >> i) & 1;
-            if (btrie[p].c[bit]) {
-                p = btrie[p].c[bit];
+            if (btrie[p][bit]) {
+                p = btrie[p][bit];
                 res = res << 1 | bit;
             } else {
                 (find_impl(p, res, 1 ^ b ^ bit, b) == 0) ? (res = (b) ? btrie_MIN : btrie_MAX) : 0;
@@ -122,8 +111,8 @@ struct TrieMap {
         int p = 0, res = 0;
         for (int i = btrie_DEPTH; i >= 0; i--) {
             int b = (x >> i) & 1;
-            res += b * btrie[btrie[p].c[0]].cnt;
-            p = btrie[p].c[b];
+            res += b * btrie[btrie[p][0]][2];
+            p = btrie[p][b];
             if (!p) { break; }
         }
         return res;
@@ -132,11 +121,11 @@ struct TrieMap {
     int find_by_order(int k) {
         int p = 0, res = 0;
         for (int i = btrie_DEPTH; i >= 0; i--) {
-            if (btrie[p].c[0] && btrie[btrie[p].c[0]].cnt > k) {
-                p = btrie[p].c[0];
+            if (btrie[p][0] && btrie[btrie[p][0]][2] > k) {
+                p = btrie[p][0];
             } else {
-                k -= btrie[btrie[p].c[0]].cnt;
-                p = btrie[p].c[1];
+                k -= btrie[btrie[p][0]][2];
+                p = btrie[p][1];
                 res |= 1 << i;
             }
         }
@@ -145,11 +134,13 @@ struct TrieMap {
 };
 
 int main() {
-    TrieMap trie{};
+    TrieMap trie;
     std::cout << "hello" << std::endl;
-    // trie.value_at(1) = 1;
-    // trie.value_at(4) = 4;
-    // trie.value_at(3) = 3;
-    // trie.value_at(2) = 2;
+    trie.value_at(1) = 1;
+    trie.value_at(4) = 4;
+    trie.value_at(3) = 3;
+    trie.value_at(-1) = 2;
+
+    std::cout << trie.find_by_order(2) << std::endl;
     return 0;
 }
